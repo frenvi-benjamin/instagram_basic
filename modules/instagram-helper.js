1c5e549e7520fbdd593e86235c5a395af0f7a580
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-04-29 11:59:11 +0200

    debug

--- modules/instagram-helper.js
+++ modules/instagram-helper.js
@@ -5,6 +5,7 @@ function getUserByInstagramAccessToken(accessToken) {
     return fetch(`https://graph.instagram.com/me?fields=id,username&access_token=${accessToken}`)
     .then(response => response.json())
     .then(body => {
+        console.log(body)
         const instagramUserID = body.id
         return userHelper.getUserByInstagramUserID(instagramUserID)
         .then(user => {return user})
