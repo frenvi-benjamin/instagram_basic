28e5b236b944f39d84ef8d218714c208a70e7b3f
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-05-06 17:36:40 +0200

    dbHelper initialises nrOfScans with 0

--- modules/db-helper.js
+++ modules/db-helper.js
@@ -45,7 +45,7 @@ function createUserFromAccessToken(accessToken) {
     ])
     .then(res => {console.log(res); return res})
     .then(([username, id, shortcode]) => {
-        return User.findOneAndUpdate({ instagramUserID: id }, { username: username, accessToken: accessToken, shortcode: shortcode, qrcodes: [] }, { upsert: true })
+        return User.findOneAndUpdate({ instagramUserID: id }, { username: username, accessToken: accessToken, shortcode: shortcode, qrcodes: [], nrOfScans: 0 }, { upsert: true })
     })
 }
 
