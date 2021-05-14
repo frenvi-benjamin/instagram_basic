3499c6579865f50414213ca902944c61c623792c
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-05-14 10:01:34 +0200

    debug logs added for scanner session fix

--- app.js
+++ app.js
@@ -332,6 +332,7 @@ app.use((req, res, next) => {                                                   
 
 app.get("/scanner", (req, res) => {
     const hasVisitedScanner = req.session.hasVisitedScanner | false
+    console.log("const", hasVisitedScanner, "sess", req.session.hasVisitedScanner)
     res.render("scanner", { instagramUserID: req.session.instagramUserID, accessToken: req.session.accessToken, username: req.session.username, hasVisitedScanner: hasVisitedScanner })
     req.session.hasVisitedScanner = true
 })
