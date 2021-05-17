7fdfe8132a8304d076e09a44b358a2c5efc67a8b
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-05-17 15:43:20 +0200

    Revert "added debug logs to shared controller"
    This reverts commit ee2ba7b6502b3fe19aaab219287a526d155da33a.

--- controllers/shared.js
+++ controllers/shared.js
@@ -1,10 +1,8 @@
 function checkForUserSession(req, res, next) {
-    if (req.session.username) {
-        console.log("Active user session. Username: ", req.session.username)
-        return next()
-    }
+    if (
+        req.session.username
+    ) { return next() }
     else {
-        console.log("No active user session. Redirecting to Login")
         res.redirect("/")
     }
 }
