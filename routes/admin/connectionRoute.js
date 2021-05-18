0eaf65810f4e57123bf989a7926727dadf454180
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-05-18 10:32:41 +0200

    changed middleware usage in admin routes

--- routes/admin/connectionRoute.js
+++ routes/admin/connectionRoute.js
@@ -2,9 +2,10 @@ const express = require("express")
 const router = express.Router()
 
 const controller = require("../../controllers/admin/connectionController")
+const adminController = require("../../controllers/adminController")
 const sharedController = require("../../controllers/sharedController")
 
-router.post("/clear", controller.clear)
+router.post("/clear", adminController.checkAuth, controller.clear)
 
 // user session check middleware
 // all routes below require an active session. if no session is active the user will be redirected to the login page
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -6,6 +6,11 @@ const controller = require("../controllers/adminController")
 //auth
 router.post("/auth", controller.authLogin)
 
+// connections
+// needs to be before admin check to allow normal users to connect their 
+// qrcodes to their account
+router.use("/connection", require("./admin/connectionRoute"))
+
 // check for admin user
 router.use(controller.checkAuth)
 
@@ -17,8 +22,6 @@ router.get("/", (req, res) => {
 // user
 router.use("/user", require("./admin/userRoute"))
 
-// connections
-router.use("/connection", require("./admin/connectionRoute"))
 
 // qrcodes
 router.use("/qrcode", require("./admin/qrcodeRoute"))
