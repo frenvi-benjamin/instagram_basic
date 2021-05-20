e25ea28b56b7297e84671ee5c8cc94172fc6f1c8
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-05-20 15:43:42 +0200

    small fix in chooseController

--- controllers/chooseController.js
+++ controllers/chooseController.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch")
 
 function render(req, res) {
-    fetch(`https://graph.instagram.com/me/media?fields=media_url&access_token=req.session.accessToken`)
+    fetch(`https://graph.instagram.com/me/media?fields=media_url&access_token=${req.session.accessToken}`)
     .then(response => response.json())
     .then(body => res.render("choose", { media: body.data }))
 }
