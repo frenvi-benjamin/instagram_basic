07557e94f4d6038984a4530a2760ac77ddef9804
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-05-31 10:17:11 +0200

    fix

--- controllers/chooseController.js
+++ controllers/chooseController.js
@@ -3,13 +3,16 @@ const fetch = require("node-fetch")
 function render(req, res) {
     fetch(`https://graph.instagram.com/me/media?fields=media_url&access_token=${req.session.accessToken}`)
     .then(response => response.json())
-    .then(body => res.render("choose", { media: body.data }))
-    .catch((err) => {
-        console.log(err)
-        res.render("request-permissions", {
-            instagramAppID: process.env.INSTAGRAM_APP_ID,
-            oauthRedirectURI: process.env.HOST + req.originalUrl
-        })
+    .then(body => {
+        if (body.data) {
+            res.render("choose", { media: body.data })
+        }
+        else {
+            res.render("request-permissions", {
+                instagramAppID: process.env.INSTAGRAM_APP_ID,
+                oauthRedirectURI: process.env.HOST + req.originalUrl
+            })
+        }
     })
 }
 
