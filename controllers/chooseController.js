a9afdc956177407fc99dc9cbd330ffa6f2f4798a
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-05-31 10:11:28 +0200

    app asks for instagram permissions again if choose page is accessed without them

--- controllers/chooseController.js
+++ controllers/chooseController.js
@@ -4,6 +4,13 @@ function render(req, res) {
     fetch(`https://graph.instagram.com/me/media?fields=media_url&access_token=${req.session.accessToken}`)
     .then(response => response.json())
     .then(body => res.render("choose", { media: body.data }))
+    .catch((err) => {
+        console.log(err)
+        res.render("request-permissions", {
+            instagramAppID: process.env.INSTAGRAM_APP_ID,
+            oauthRedirectURI: process.env.HOST + req.originalUrl
+        })
+    })
 }
 
 module.exports = { render }
