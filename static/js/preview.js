a2ac1e4a184bed2339440e8897b014bdfda7d62c
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-07-02 12:38:31 +0200

    resize preview page iframe on page resize

--- static/js/preview.js
+++ static/js/preview.js
@@ -2,6 +2,8 @@ const iframe = document.getElementById("iframe")
 
 setInterval(resizeIframe, 1000);
 
+addEventListener("resize", resizeIframe)
+
 function resizeIframe() {
-    iframe.style.height = (iframe.contentWindow.document.body.scrollHeight + 100)+"px"
+    iframe.style.height = (iframe.contentWindow.document.body.scrollHeight + 50)+"px"
 }
