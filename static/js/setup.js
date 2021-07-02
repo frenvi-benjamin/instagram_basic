42e2a1dfafb369c00f358c45a82bbb6ad47f0375
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-07-02 11:53:38 +0200

    dynamically get number of content pages on setup page

--- static/js/setup.js
+++ static/js/setup.js
@@ -1,6 +1,5 @@
 const nextButtons = document.getElementsByClassName("next")
 const prevButtons = document.getElementsByClassName("prev")
-const title = document.getElementById("title")
 const progressBar = document.getElementsByClassName("progress-bar")[0]
 
 for (let i = 0; i < nextButtons.length; i++) {
@@ -19,55 +18,27 @@ for (let i = 0; i < prevButtons.length; i++) {
     })
 }
 
-const pages = [
-    {
-        id: "choose-page",
-        title: "Post auswählen"
-    },
-    {
-        id: "reward-page",
-        title: "Goodie auswählen"
-    },
-    {
-        id: "scanner-page",
-        title: "QR-Codes scannen"
-    },
-    {
-        id: "preview-page",
-        title: "Vorschau der Kampagne"
-    }
-]
+const pages = document.getElementsByClassName("setup-content-page")
 
 const FIRST_PAGE = 0
 const LAST_PAGE = pages.length - 1
 
 var currentPage = FIRST_PAGE
 
-title.innerText = pages[currentPage].title
-document.getElementById(pages[currentPage].id).hidden = false
+pages[currentPage].hidden = false
 
 function changePage(nr) {
     const previousPage = currentPage
     currentPage += nr
 
-    // transition title
-
-    const titleTimeline = gsap.timeline()
-    titleTimeline.to(title, { duration: 0.25, opacity: 0 })
-    titleTimeline.to(title, { duration: 0, innerText: pages[currentPage].title }) 
-    titleTimeline.to(title, { duration: 0.25, opacity: 1 })
-
     // transition content
 
-    const previousContent = document.getElementById(pages[previousPage].id)
-    const currentContent = document.getElementById(pages[currentPage].id)
-
     const contentTimeline = gsap.timeline()
-    contentTimeline.to(previousContent, { duration: 0.25, opacity: 0 })
-    contentTimeline.to(previousContent, { duration: 0, hidden: true })
+    contentTimeline.to(pages[previousPage], { duration: 0.25, opacity: 0 })
+    contentTimeline.to(pages[previousPage], { duration: 0, hidden: true })
 
-    contentTimeline.to(currentContent, { duration: 0, hidden: false })
-    contentTimeline.to(currentContent, { duration: 0.25, opacity: 1 })
+    contentTimeline.to(pages[currentPage], { duration: 0, hidden: false })
+    contentTimeline.to(pages[currentPage], { duration: 0.25, opacity: 1 })
 
     // move progress bar
 
