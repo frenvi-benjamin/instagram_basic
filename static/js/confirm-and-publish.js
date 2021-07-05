56048f19d01c53d60be47bbcee1861441e546981
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-07-05 14:26:33 +0200

    comfirm and publish page almost done

--- static/js/confirm-and-publish.js
+++ static/js/confirm-and-publish.js
@@ -3,3 +3,29 @@ const changePostButton = document.getElementById("change-post")
 changePostButton.addEventListener("click", ()=> {
     jumpToPageById("choose-post-page")
 })
+
+const preview = document.getElementById("campaign-preview")
+const publishButton = document.getElementById("publish-button")
+const closePreviewButton = document.getElementById("close-preview-button")
+const showPreviewButton = document.getElementById("show-preview-button")
+
+closePreviewButton.addEventListener("click", hidePreview)
+showPreviewButton.addEventListener("click", showPreview)
+publishButton.addEventListener("click", publishCampaign)
+
+function hidePreview() {
+    const tl = gsap.timeline()
+    tl.to(preview, { duration: 0.25, opacity: 0 })
+    tl.to(preview, { duration: 0, hidden: true })
+}
+
+function showPreview() {
+    const tl = gsap.timeline()
+    tl.to(preview, { duration: 0, hidden: false })
+    tl.to(preview, { duration: 0.25, opacity: 1 })
+}
+
+function publishCampaign() {
+    // TODO set public to true
+    new bootstrap.Modal(document.getElementById("publish-modal")).show()
+}
