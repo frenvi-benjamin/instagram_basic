0c106b3203b373be7ded9e026e79df7c1e68edee
Benjamin <benjamin.mayer@frenvi-sales.de>
2021-07-05 10:06:17 +0200

    finished choose-reward page of setup

--- static/js/reward.js
+++ static/js/reward.js
@@ -1,3 +1,33 @@
+const TYPE_CUP_OF_ICE_CREAM = 1
+const TYPE_EXTRA_CREAM = 2
+const TYPE_EXTRA_SCOOP = 3
+const TYPE_SURPRISE = 4
+
+fetch("/reward/get")
+.then(response => response.json())
+.then(user => {
+    switch (user.rewardType) {
+        case TYPE_CUP_OF_ICE_CREAM:
+            document.getElementById("choice1").checked = true
+            setRewardType(user.rewardType)
+            break;
+        case TYPE_EXTRA_CREAM:
+            document.getElementById("choice2").checked = true
+            setRewardType(user.rewardType)
+            break;
+        case TYPE_EXTRA_SCOOP:
+            document.getElementById("choice3").checked = true
+            setRewardType(user.rewardType)
+            break;
+        case TYPE_SURPRISE:
+            document.getElementById("choice4").checked = true
+            setRewardType(user.rewardType)
+            break;
+    }
+})
+
+const choices = document.getElementsByName("reward")
+
 for (let i = 0; i < choices.length; i++) {
     const choice = choices[i]
     choice.addEventListener("click", () => {
@@ -20,4 +50,25 @@ for (let i = 0; i < choices.length; i++) {
             document.getElementById("preview").innerHTML = modal
         })
     })
+}
+
+var rewardType
+
+function setRewardType(type) {
+    rewardType = type
+    const preview =  document.getElementById("goodie-preview-text")
+    switch (type) {
+        case TYPE_CUP_OF_ICE_CREAM:
+            preview.innerHTML = "eine Kugel Eis im Becher mit unserem EATlery&copy; Löffel."
+            break;
+        case TYPE_EXTRA_CREAM:
+            preview.innerHTML = "eine extra Portion Sahne auf ihr Eis."
+            break;
+        case TYPE_EXTRA_SCOOP:
+            preview.innerHTML = "eine Kugel Eis extra."
+            break;
+        case TYPE_SURPRISE:
+            preview.innerHTML = "eine Überraschung. Du suchst selbst aus was deine Kunden erhalten."
+            break;
+    }
 }
