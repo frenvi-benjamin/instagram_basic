d56038b2fb6a5c6459ab467b55e8514a30ca2d56
frenvi-benjamin <benjamin.mayer@frenvi-sales.de>
2021-08-09 11:21:02 +0200

    small changes to iOS fix

--- static/js/iOSCampaign.js
+++ static/js/iOSCampaign.js
@@ -3,3 +3,7 @@ _iOSDevice = !!navigator.platform.match(/iPhone|iPod|iPad/);
 document.querySelectorAll(".white-bars>span").forEach(element => {
     element.style.lineHeight = 2.4
 })
+
+// const icon = document.querySelector("#zero-waste-icon")
+// icon.style.marginBottom = "1rem"
+// icon.style.maxWidth = "40px"
