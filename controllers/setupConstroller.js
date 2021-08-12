2919e6738d1de893ded0480554e4cd9be782d726
frenvi-benjamin <benjamin.mayer@frenvi-sales.de>
2021-08-12 15:54:31 +0200

    remove debugging logs

--- controllers/setupConstroller.js
+++ controllers/setupConstroller.js
@@ -2,23 +2,13 @@ const fetch = require("node-fetch")
 const User = require("../models/userModel")
 
 function render(req, res) {
-    console.log("starting render setup")
     fetch(`https://graph.instagram.com/me/media?fields=media_url,media_type,thumbnail_url,permalink&access_token=${req.session.accessToken}`)
     .then(response => response.json())
     .then(body => {
-        console.log("received media")
         if (body.data) {
             User.findOne({ username: req.session.username })
             .then(user => {
-                console.log("found user, starting render")
-                res.render("setup",
-                { media: body.data, rewardType: user.rewardType, promotedPost: user.promotedPost, instagramUserID: req.session.instagramUserID, accessToken: req.session.accessToken, username: req.session.username },
-                (err, html) => {
-                    console.log(html)
-                    console.log("finished render")
-                    res.send(html)
-                })
-                
+                res.render("setup", { media: body.data, rewardType: user.rewardType, promotedPost: user.promotedPost, instagramUserID: req.session.instagramUserID, accessToken: req.session.accessToken, username: req.session.username })
             })
             
         }
